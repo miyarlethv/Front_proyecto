27f922fc6833f0997b58ffb64d5e6b4faf64fe69
Karol Jessenia Castro Bedoya <karol.castro@ucp.edu.co>
2024-05-18 16:14:52 -0500

    actua

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Registro from './components/Registro';
 import Login from './components/Login';
-import login from '../assets/login.jpg';
 
 function App() {
   return (
