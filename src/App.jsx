447ad91bbb0f0f1d2c773580e26fc6833eddea4a
miyarlethv <miyarlethvarelas@gmail.com>
2024-05-19 18:44:16 -0500

    app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Registro from './components/Registro';
 import Login from './components/Login';
 import Menu from './components/Menu';
+import Product from './components/Product';
 
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
         <Route path="/" element={<Registro />} />
         <Route path="/form2" element={<Login />} />
         <Route path="" element={<Menu />} />
-     
+        <Route path="" element={<Product />} />
+    
       </Routes>
     </Router>
   );
