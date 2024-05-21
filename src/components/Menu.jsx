1459ad306bd46e691265843b1763f7f2d91a933f
miyarlethv <miyarlethvarelas@gmail.com>
2024-05-20 20:17:07 -0500

    todo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,15 +5,16 @@ import '../style/style.css';
 const Menu = ({ products }) => {
   return (
     <div className="menu">
-      {products.map((product) => (
-        <Product 
-          key={product.id} 
-          name={product.name} 
-          description={product.description} 
-          price={product.price} 
-        />
-      ))}
-    </div>
+    <h1>PRODUCTOS </h1> 
+    {products.map((product) => (
+      <Product 
+        key={product.id} 
+        name={product.name} 
+        description={product.description} 
+        price={product.price} 
+      />
+    ))}
+  </div>
   );
 };
 
